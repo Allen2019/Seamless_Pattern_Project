d21d0acef87f0c5c6887774186d41a820238bf8b
Allen2019 <allen.shaw.2019@gmail.com>
2019-01-05 19:56:04 +0800

    Begin Project

---
+++ Seamless Pattern/Seamless Pattern/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Seamless Pattern
+//
+//  Created by allen01px2019 on 2019/1/5.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Seamless Pattern/Seamless Pattern/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Seamless Pattern
+//
+//  Created by allen01px2019 on 2019/1/5.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
